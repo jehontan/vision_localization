a630583c80d8b0c52176f0748c175dcbc0233708
jehon <jehontan@gmail.com>
2022-06-02 14:52:45 +0800

    Add fixed marker selection policy

--- src/vision_localization/node.py
+++ src/vision_localization/node.py
@@ -79,7 +79,8 @@ def main():
     parser.add_argument('config', type=str, help='Configuration file.')
     parser.add_argument('camera', type=str, help='Camera name.')
     parser.add_argument('-i', dest='camera_num', required=False, type=int, default=0, help='Camera input number. Default: 0')
-    parser.add_argument('--log', dest='log_level', type=str, default='info', required=False, help='Logging level. [debug|info|warn|error|critical]')
+    parser.add_argument('--policy', dest='policy', required=False, type=str, default='first', help='Fixed marker selection policy. [first|closest|best]. Default: first')
+    parser.add_argument('--log', dest='log_level', type=str, default='info', required=False, help='Logging level. [debug|info|warn|error|critical]. Default: info')
 
     args = parser.parse_args()
 
@@ -96,6 +97,15 @@ def main():
     logging.basicConfig(level=map_log_level[args.log_level],
                 format='[%(levelname)5s][%(asctime)s][%(name)s]: %(message)s',
                 datefmt='%H:%M:%S')
+
+    # setup fixed marker selection policy
+    map_select_policy = {
+        'first': 0,
+        'closest': 1,
+        'best': 2,
+    }
+
+    select_policy = map_select_policy[args.policy]
 
     load_config(args)
 
@@ -118,6 +128,11 @@ def main():
             
             corners, ids, _ = cv2.aruco.detectMarkers(frame, marker_dict)
 
+            if select_policy == 1:
+                centers = [np.average(c[0], axis=0) for c in corners]
+            elif select_policy == 2:
+                quality = [abs((c[0][0,:]-c[0][2:0])/(c[0][1,:]-c[0][3,:])) for c in corners] 
+
             if not corners:
                 # no markers, early terminate
                 logging.warn('No markers found.')
@@ -138,20 +153,34 @@ def main():
                     if id_ in roaming_markers:
                         roam_inds.append(i)
 
-                # just pick first fixed marker as reference
-
                 if not fixed_inds:
                     logging.warn('No fixed markers seen.')
                 else:
-                    fixed_ind = fixed_inds[0]
-                    origin_to_fixed = fixed_markers[ids_[fixed_ind]]
-                    fixed_to_cam = transformation(rvecs[fixed_ind], tvecs[fixed_ind])
-                    origin_to_cam = fixed_to_cam @ origin_to_fixed
-
                     # iterate over observed roaming markers
                     roaming_poses = dict()
+                    
+                    if select_policy == 1:
+                        fixed_centers = [centers[f] for f in fixed_inds]
+                    elif select_policy == 2:
+                        fixed_quality = [quality[f] for f in fixed_inds]
 
                     for roam_ind in roam_inds:
+                        # pick fixed marker reference based on policy
+                        if select_policy == 0:
+                            # default to picking first seen fixed marker
+                            fixed_ind = fixed_inds[0]
+                        elif select_policy == 1:
+                            # pick closest marker
+                            distances = np.linalg.norm(np.subtract(fixed_centers, centers[roam_ind][0]), axis=1)
+                            fixed_ind = fixed_inds[np.argmin(distances)]
+                        elif select_policy ==2:
+                            # use best marker                            
+                            fixed_ind = fixed_inds[np.argmax(fixed_quality)]
+
+                        origin_to_fixed = fixed_markers[ids_[fixed_ind]]
+                        fixed_to_cam = transformation(rvecs[fixed_ind], tvecs[fixed_ind])
+                        origin_to_cam = fixed_to_cam @ origin_to_fixed
+
                         robot_to_cam = transformation(rvecs[roam_ind], tvecs[roam_ind])
 
                         robot_to_origin = np.linalg.inv(origin_to_cam)@robot_to_cam
